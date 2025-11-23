254b0e2c9cc0dc0caa9fb3a4ca0746dc96e5ee16
Devourer-of-universes <artem.degterev1101@gmail.com>
2025-11-23 10:19:47 +0700

    ver 23.11.25.1 structure

--- app/src/main/java/com/example/myapplication/screen/Login/signIn/SignInScreen.kt
+++ app/src/main/java/com/example/myapplication/screen/Login/signIn/SignInScreen.kt
@@ -1,2 +1,139 @@
 package com.example.myapplication.screen.Login.signIn
 
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import com.example.myapplication.R
+import com.example.myapplication.ui.theme.bgGreyDark
+import com.example.myapplication.ui.theme.txtGreyLight
+import com.example.myapplication.ui.theme.txtMainSelected
+import com.example.myapplication.ui.theme.txtMainWhite
+
+
+@Composable
+fun SignInScreen(navController: NavHostController){
+    val viewModel: SignInViewModel = hiltViewModel()
+    val uiState = viewModel.state.collectAsState()
+
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    val context = LocalContext.current
+    LaunchedEffect(key1 = uiState.value) {
+        when(uiState.value){
+            is SignInState.Success -> {
+                navController.navigate(route = "app")
+            }
+
+            is SignInState.Error -> {
+                Toast.makeText(context, "Ошибка авторизации", Toast.LENGTH_SHORT).show()
+            }
+            else -> {}
+
+        }
+
+    }
+    Column (modifier = Modifier
+        .fillMaxWidth()
+        .background(color = bgGreyDark)
+        .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        Spacer(modifier = Modifier.padding(50.dp))
+        Text(
+            text = "Добро пожаловать!",
+            color = txtMainSelected,
+            fontSize = 20.sp
+        )
+        Text(
+            text = "Войдите в свой аккаунт",
+            color = txtMainSelected,
+            fontSize = 20.sp
+        )
+        Spacer(modifier = Modifier.padding(50.dp))
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            placeholder = {Text("Электронная почта", color = txtGreyLight)},
+            modifier = Modifier
+                .fillMaxWidth(0.8f)
+                .padding(bottom = 20.dp),
+            shape = RoundedCornerShape(50),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor= txtMainSelected,
+                unfocusedBorderColor = txtMainSelected
+            )
+        )
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            placeholder = {Text("Пароль", color = txtGreyLight)},
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier
+                .fillMaxWidth(0.8f),
+            shape = RoundedCornerShape(50),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor= txtMainSelected,
+                unfocusedBorderColor = txtMainSelected,
+
+                )
+        )
+        Text(
+            text = "Забыли пароль?",
+            color = txtGreyLight,
+            fontSize = 12.sp,
+            textAlign = TextAlign.Right,
+            modifier = Modifier
+                .fillMaxWidth(0.8f)
+                .padding(bottom = 40.dp)
+
+        )
+        Button(
+            onClick = {},
+            modifier = Modifier.size(width = 220.dp, height = 48.dp).padding(bottom = 10.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = txtMainSelected
+            )
+        ) {
+            Text(
+                text = "Авторизация",
+                fontSize = 18.sp,
+                color = txtMainWhite
+            )
+        }
+        Text(
+            text = "У вас нет учётной записи? Регистрация",
+            color = txtGreyLight,
+            fontSize = 12.sp
+        )
+    }
+}
--- app/src/main/java/com/example/myapplication/screen/Login/signUp/SignUpScreen.kt
+++ app/src/main/java/com/example/myapplication/screen/Login/signUp/SignUpScreen.kt
@@ -1,2 +1,142 @@
 package com.example.myapplication.screen.Login.signUp
 
+@Composable
+fun SignUpScreen(navController: NavHostController) {
+
+    val viewModel: SignUpViewModel = hiltViewModel()
+    val uiState = viewModel.state.collectAsState()
+
+    var name by remember {
+        mutableStateOf("")
+    }
+    var email by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
+    var confirmPassword by remember {
+        mutableStateOf("")
+    }
+
+    val context = LocalContext.current
+    LaunchedEffect(key1 = uiState.value) {
+        when (uiState.value) {
+            is SignUpState.Success -> {
+                navController.navigate(route = "app")
+            }
+
+            is SignUpState.Error -> {
+                Toast.makeText(context, "Ошибка регистрации", Toast.LENGTH_SHORT).show()
+            }
+
+            else -> {}
+
+        }
+
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(bgGreyDark),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(
+            modifier = Modifier
+                .padding(top = 150.dp)
+        )
+
+
+        Text(
+
+            color = txtIcMainSelected,
+            text = "Давайте начнем!",
+            fontWeight = W700,
+            fontSize = 22.sp
+        )
+        Spacer(
+            modifier = Modifier
+                .padding(top = 10.dp)
+        )
+        Text(
+            color = txtIcMainSelected,
+            text = "Зарегистрируйте свой новый аккаунт",
+            fontWeight = W700,
+            fontSize = 20.sp
+        )
+
+        var isError by remember {
+            mutableStateOf(false)
+        }
+        isError = password != confirmPassword
+
+        Spacer(
+            modifier = Modifier
+                .padding(top = 100.dp)
+        )
+
+        FirmOutlineTextField(
+            label = "Имя пользователя",
+            value = { name = it },
+            paddingTop = 10.dp
+        )
+        FirmOutlineTextField(
+            label = "Электронная почта",
+            value = { email = it },
+            paddingTop = 10.dp
+        )
+        FirmOutlineTextField(
+            label = "Пароль",
+            value = { password = it },
+            paddingTop = 10.dp,
+            password = true
+        )
+        FirmOutlineTextField(
+            label = "Подтвердите пароль",
+            value = { confirmPassword = it },
+            paddingTop = 10.dp,
+            password = true,
+            error = isError
+        )
+
+        if (uiState.value == SignUpState.Loading) {
+            CircularProgressIndicator(
+                color = txtIcMainSelected,
+                modifier = Modifier
+                    .padding(top = 40.dp)
+            )
+        } else {
+            Button(
+                modifier = Modifier
+                    .padding(top = 40.dp),
+                colors = btnMainOrange,
+                onClick = {
+                    viewModel.signUp(
+                        name = name,
+                        email = email,
+                        password = password
+                    )
+                },
+                enabled = name.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty() && password == confirmPassword
+            ) {
+                Text(
+                    modifier = Modifier
+                        .padding(horizontal = 40.dp, vertical = 5.dp),
+                    fontWeight = W700,
+                    fontSize = 18.sp,
+                    text = "Регистрация"
+                )
+            }
+        }
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                color = txtIcMainGrey,
+                text = "У вас есть аккаунт?"
+            )
+            TextButton(onClick = { navController.popBackStack() }) {
+                Text(fontWeight = W700, color = txtIcMainGrey, text = "Авторизация")
+            }
+        }
+    }
+}
